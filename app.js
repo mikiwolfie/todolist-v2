c97f19d44dfac97ebb89bda6d13be9f987c5bff7
unknown <samanthamikhail24@gmail.com>
2021-01-20 09:55:33 +0800

    Initial commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://mikiwolfie:test123@cluster0.ywdcy.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
